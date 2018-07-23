aec8122523fd3e06526ff97f214d514ad32125ab
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-07-23 23:12:21 +0200

    Added NodeJS Collatz Conjecture

---
+++ collatz.js
@@ -0,0 +1,19 @@
+let num = 1
+let counterMax = 0
+while (true) {
+    calc = num
+    counter = 1
+    while (calc != 1) {
+        if (calc % 2 == 0) {
+            calc = parseInt(calc / 2)
+        } else {
+            calc = parseInt(3 * calc + 1)
+        }
+        counter++
+    }
+    if (counterMax < counter) {
+        counterMax = counter
+        console.log(`Collatz from ${num} of len ${counter}`)
+    }
+    num++
+}
